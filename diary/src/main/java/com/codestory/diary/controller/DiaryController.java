f93e88524cbde10881c5aa548ba8a934ee28788d
[Jiin5906] <[ooojiin@gmail.com]>
2026-01-16 13:22:28 +0900

    처음부터 시작

--- diary/src/main/java/com/codestory/diary/controller/DiaryController.java
+++ diary/src/main/java/com/codestory/diary/controller/DiaryController.java
@@ -1,17 +1,32 @@
 package com.codestory.diary.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.codestory.diary.dto.DiaryDto;
 import com.codestory.diary.dto.DiaryRequestDto;
 import com.codestory.diary.service.DiaryService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173") 
+@CrossOrigin(origins = {
+    "http://localhost:5173",
+    "http://logam.click",
+    "http://43.202.111.91"
+})
 public class DiaryController {
 
     private final DiaryService diaryService;
