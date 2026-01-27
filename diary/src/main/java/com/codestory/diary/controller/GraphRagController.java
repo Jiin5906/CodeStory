b7c02bb24cf17d56d3af8ac7b5f8814efd805740
[Jiin5906] <[ooojiin@gmail.com]>
2026-01-27 13:33:58 +0900

    404에러 수정

--- diary/src/main/java/com/codestory/diary/controller/GraphRagController.java
+++ diary/src/main/java/com/codestory/diary/controller/GraphRagController.java
@@ -2,6 +2,7 @@ package com.codestory.diary.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import com.codestory.diary.service.GraphRagService;
 import com.codestory.diary.neo4j.GraphService;
 
 @RestController
+@RequestMapping("/api")
 public class GraphRagController {
 
     @Autowired
